4c39f219a659391a1107dbf528de9e1387cdc3a9
KyungMin_ <tommykmin@naver.com>
2021-03-17 02:12:12 +0900

    [ADD] commit

--- src/main/java/tommy/spring/web/common/PointcutCommon.java
+++ src/main/java/tommy/spring/web/common/PointcutCommon.java
@@ -2,20 +2,12 @@ package tommy.spring.web.common;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Service;
-
-@Service
-@Aspect // Aspect = Pointcut + Advice
-public class LogAdvice {
+@Aspect
+public class PointcutCommon {
 	@Pointcut("execution(* tommy.spring.web..*Impl.*(..))")
 	public void allPointcut() {
 	}
 	@Pointcut("execution(* tommy.spring.web..*Impl.get*(..))")
 	public void getPointcut() {
-	}
-	@Bean("allPointcut()")
-	public void printLog() {
-		System.out.println("[공통 로그] : 비즈니스 로직 수행 전 동작");
 	}
 }
